00d23a0d0f7a4d92c9280b02b762a6c0055298d0
KK <kk@unit9.com>
2018-06-25 19:08:09 +0200

    day1 part2 + divided some super small amount of logic

--- src/Day1/main.kt
+++ src/Day1/main.kt
@@ -1,15 +1,38 @@
 package Day1
 
-fun calculate(data: String): Int{
+fun String.parseToIntMutableList(): List<Int> {
+    return this.map { it.toString().toInt() }
+}
+
+// Part I
+fun calculate(data: String): Int {
     // Split it nicely
-    val digits: MutableList<Int> = data.map { it.toString().toInt() }.toMutableList()
+    val digits = data.parseToIntMutableList()
     // Calculate rest
-    return digits.foldIndexed(0) {
-        idx: Int, acc: Int, i: Int -> if (i == digits[(idx + 1) % digits.size])acc + i else acc
+    return digits.foldIndexed(0) { idx: Int, acc: Int, i: Int ->
+        if (i == digits[(idx + 1) % digits.size]) acc + i else acc
     }
 }
 
-fun main(args: Array<String>){
-    val res = calculate("Your code here")
-    println(res)
+// Part II
+fun calculate2(data: String): Int {
+    // Split it nicely
+    val digits = data.parseToIntMutableList()
+    // Calculate rest
+    return digits.foldIndexed(0) { idx: Int, acc: Int, i: Int ->
+        if (i == digits[(idx + digits.size / 2) % digits.size]) acc + i else acc
+    }
+}
+
+
+fun main(args: Array<String>) {
+    var data = "1233211"
+
+    // Part I
+    val res = calculate(data)
+    println("Part I: $res")
+
+    // Part II
+    val res2 = calculate2(data)
+    println("Part II: $res2")
 }
--- test/Day1/testMain.kt
+++ test/Day1/testMain.kt
@@ -6,11 +6,18 @@ import kotlin.test.assertEquals
 
 class TestDay1{
 
-    @Test fun `Day 1 ╯°□°）╯`() {
+    @Test fun `Day 1 Part I  ╯°□°）╯`() {
         assertEquals(calculate("1122"), 3)
         assertEquals(calculate("1111"), 4)
         assertEquals(calculate("1234"), 0)
         assertEquals(calculate("91212129"), 9)
     }
 
+    @Test fun `Day 1 Part II  ╯°□°）╯`() {
+        assertEquals(calculate2("1212"), 6)
+        assertEquals(calculate2("1221"), 0)
+        assertEquals(calculate2("123425"), 4)
+        assertEquals(calculate2("123123"), 12)
+        assertEquals(calculate2("12131415"), 4)
+    }
 }
